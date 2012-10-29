ab442872091ddc5e3214c34f083d6e958f6f608a
Javier Mey <javier.mey@sage.com>
2012-10-29 10:21:25 +0100

    Added coverage function with test working 100%

--- lib/difflation/compare.rb
+++ lib/difflation/compare.rb
@@ -4,8 +4,22 @@ module Difflation
       compare_hash(origin, destination)
     end
 
-    def self.coverage
-      0.0
+    # Get Coverage rate from two hashes, depending on the number of keys that have a given token
+    #
+    # @param [Hash] origin I18n source translation to compare
+    # @param [Hash] destination I18n target translation to compare
+    # @param [String] token The string you want to compare. example: **NOT TRANSLATED**
+    def self.coverage(origin, destination, token = "**NOT TRANSLATED**")
+      words = 0
+      found = 0
+      return "100.00% #{found}/#{words} entries translated"  if origin.nil?
+      destination = {} if destination.nil?
+      return "100.00% #{found}/#{words} entries translated"  if origin.empty?
+
+      words, found = rate_from_branch(origin,destination, token, words, found)
+      percent = (found.to_f/words.to_f) * 100
+      truncate = "%.2f" % percent
+      return "#{truncate}% #{found}/#{words} entries translated"
     end
 
     private
@@ -24,6 +38,26 @@ module Difflation
         end
       end
       destination
+    end
+
+    def self.rate_from_branch(origin, destination, token, words, found)
+      origin.each_pair{ |key, value|
+        if origin[key.to_sym].instance_of? Hash
+          if destination[key.to_sym]
+            words, found = rate_from_branch(origin[key.to_sym], destination[key.to_sym], token, words, found)
+          else
+            # Sum other words
+            # could have nested branches, so we call it with origin[key.to_sym] to count the number of words, returning the found to a temporal var
+            words, temp = rate_from_branch(origin[key.to_sym], origin[key.to_sym], token, words, found)
+          end
+        else
+          words += 1
+          if destination[key.to_sym]
+            found += 1 if !destination[key.to_sym].to_s.include?(token)
+          end
+        end
+      }
+      return words, found
     end
   end
 end
